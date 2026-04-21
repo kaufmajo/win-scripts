bbd9df1984d054e24dcbed48d8bd39babd3c8803
Joachim Kaufmann <joachim_kaufmann@bluewin.ch>
2026-04-21 16:24:10 +0200

    Refactor Write-MainHeader function to standardize border formatting and improve visual consistency

--- library/function/Function_Write-MainHeader.ps1
+++ library/function/Function_Write-MainHeader.ps1
@@ -13,13 +13,13 @@ function Write-MainHeader {
     )
 
     $innerWidth = $Width - 2
-    $outerBorder = "//" + ("=" * ($Width - 2)) + "\\"
-    $emptyLine = "| " + (" " * $innerWidth) + " |"
+    $outerBorder = ("=" * ($Width))
+    $emptyLine = "|" + (" " * $innerWidth) + "|"
     $titleText = "[ $Title ]"
     $subtitleText = "< $Subtitle >"
-    $titleLine = "| " + $titleText.PadLeft([Math]::Floor(($innerWidth + $titleText.Length) / 2)).PadRight($innerWidth) + " |"
-    $subtitleLine = "| " + $subtitleText.PadLeft([Math]::Floor(($innerWidth + $subtitleText.Length) / 2)).PadRight($innerWidth) + " |"
-    $footerBorder = "\\" + ("=" * ($Width - 2)) + "//"
+    $titleLine = "|" + $titleText.PadLeft([Math]::Floor(($innerWidth + $titleText.Length) / 2)).PadRight($innerWidth) + "|"
+    $subtitleLine = "|" + $subtitleText.PadLeft([Math]::Floor(($innerWidth + $subtitleText.Length) / 2)).PadRight($innerWidth) + "|"
+    $footerBorder = ("=" * ($Width))
 
     Write-Host
     Write-Host $outerBorder -ForegroundColor $AccentColor
